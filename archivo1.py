64040004b99112a1c7f8ec71466e50b0957520ef
Alejo <80418452+AlejoTorres2001@users.noreply.github.com>
2021-11-13 16:38:48 -0300

    ...

--- archivo1.py
+++ archivo1.py
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 print("Alejo Torres!")
 print ("IITA 2021")
 hola=12
-=======
 print("Hola IITA! Python2021")
 print("El mejor curso del mundo")
 
->>>>>>> 67c81012b2de1aec98ffc45e00d7e2008258b747
 
 
 
